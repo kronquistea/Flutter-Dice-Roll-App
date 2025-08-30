57f21819899ca8da291ee167c845e83e8563c2d4
Erik Kronquist <erik.kronquist@ucdenver.edu>
2025-08-30 16:45:30 -0600

    Implement "challenge" - font size: 28 and font color: white

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,13 @@ void main() {
             ),
           ),
           child: const Center(
-            child: Text('Hello World'),
+            child: Text(
+              'Hello World',
+              style: TextStyle(
+                fontSize: 28,
+                color: Colors.white,
+              ),
+            ),
           ),
         ),
       ),
