96bdbc76a47061df92f3f6c364a6282be7168965
Erik Kronquist <erik.kronquist@ucdenver.edu>
2025-08-31 20:23:04 -0600

    End Lecture 50

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,27 +1,23 @@
 import 'package:flutter/material.dart';
 
+import 'package:first_app/dice_roller.dart';
+
 const startAlignment = Alignment.topLeft;
 const endAlignment = AlignmentGeometry.bottomRight;
 
 class GradientContainer extends StatelessWidget {
-  GradientContainer(this.color1, this.color2, {super.key});
+  const GradientContainer(this.color1, this.color2, {super.key});
 
-  GradientContainer.purple({super.key}) 
+  const GradientContainer.purple({super.key}) 
     : color1 = Colors.deepPurple, 
       color2 = Colors.indigo;
   
-  GradientContainer.orange({super.key}) 
+  const GradientContainer.orange({super.key}) 
     : color1 = const Color.fromARGB(255, 255, 60, 0), 
       color2 = const Color.fromARGB(255, 255, 128, 89);
 
   final Color color1;
   final Color color2;
-  var activeDiceImage = 'assets/images/dice-1.png';
-
-  void rollDice() {
-    activeDiceImage = 'assets/images/dice-2.png';
-    print('Changed image');
-  }
 
   @override
   Widget build(context) {
@@ -37,31 +33,7 @@ class GradientContainer extends StatelessWidget {
         ),
       ),
       child: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Image.asset(
-              activeDiceImage, 
-              width: 200,
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            TextButton(
-              onPressed: rollDice, 
-              style: TextButton.styleFrom(
-                // padding: const EdgeInsets.only(
-                //   top: 20,
-                // ),
-                foregroundColor: Colors.white,
-                textStyle: const TextStyle(
-                  fontSize: 28,
-                ),
-              ),
-              child: const Text('Roll Dice!'),
-            ),
-          ],
-        ), 
+        child: DiceRoller(),
       ),
     );
   }
