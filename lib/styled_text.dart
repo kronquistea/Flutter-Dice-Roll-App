e856854a50afaea4dc80e007654a11ccbbf6f200
Erik Kronquist <erik.kronquist@ucdenver.edu>
2025-08-31 18:58:05 -0600

    End lecture 44

--- lib/styled_text.dart
+++ lib/styled_text.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 
 class StyledText extends StatelessWidget {
-  const StyledText({super.key});
+  // Should maintain const in front of constructor because text will never change (final)
+  const StyledText(this.text, {super.key});
   
+  final String text;
+
   @override
   Widget build(context) {
     return Text(
-      'Hello World!',
+      text,
       style: TextStyle(
         color: Colors.white,
         fontSize: 28,
