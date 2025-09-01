af4188371e8ac0018a730420d432aa27ece534ea
Erik Kronquist <erik.kronquist@ucdenver.edu>
2025-08-31 19:18:03 -0600

    Lecture 45 main solution 2

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ void main() {
   runApp(
     const MaterialApp(
       home: Scaffold(
-        body: GradientContainer(colors: [Color.fromARGB(255, 255, 102, 0), Color.fromARGB(255, 248, 149, 83)]),
+        body: GradientContainer(Color.fromARGB(255, 255, 102, 0), Color.fromARGB(255, 248, 149, 83)),
       ),
     ),
   );
